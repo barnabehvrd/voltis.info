f4f1e5bba0b343477e53ccca8b4079d006265f05
v0ltis <voltis.discord@gmail.com>
2025-03-09 11:19:19 +0100

    Add animated background effects and responsive styles

--- src/assets/scripts/scripts.js
+++ src/assets/scripts/scripts.js
@@ -18,3 +18,22 @@
     document.getElementById('currYear').innerText = today.getFullYear();
 })();
 
+(() => {
+    particlesJS('particles-js', {
+        "particles": {
+            "number": {"value": 80, "density": {"enable": true, "value_area": 800}},
+            "color": {"value": "#8A4FFF"},
+            "shape": {"type": "circle"},
+            "opacity": {"value": 0.35, "random": false},
+            "size": {"value": 3, "random": true},
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#8A4FFF",
+                "opacity": 0.25,
+                "width": 1.25
+            },
+            "move": {"enable": true, "speed": 0.175, "direction": "top", "random": true}
+        }
+    });
+})();
